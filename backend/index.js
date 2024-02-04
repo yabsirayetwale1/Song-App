383c298b1970fc806ffae3bf1c14fcab85a3ce5f
yabsirayetwale1 <158767746+yabsirayetwale1@users.noreply.github.com>
2024-02-04 03:28:50 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-app.use(postRoutes);
+app.use('/songs',postRoutes);
 
 const PORT = process.env.PORT|| 5000;
 const CONNECTION_URL = 'mongodb+srv://yabsira:yabsira@cluster0.uvc1l0s.mongodb.net/';
-// const CONNECTION_URL = "mongodb://localhost:27017/mobileapp";
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
